bc6c2d1fd9c3ecd58c9bb7d90f5e510c7aef5f01
--global <--global>
2023-07-11 21:56:23 +0700

    feat: use filepath.Walk and categorized output

--- convert.go
+++ convert.go
@@ -3,35 +3,77 @@ package main
 import (
 	"fmt"
 	"image/png"
+	"log"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/hallazzang/gosang"
 )
 
 func main() {
-	f, err := os.Open(`data\AdvancedSamun_M.S32`)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	sp, err := gosang.OpenSprite(f)
-	if err != nil {
-		panic(err)
-	}
-	for i := 0; i < sp.FrameCount(); i++ {
-		frame, err := sp.Frame(i)
+	err := filepath.Walk(`data`, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
+			log.Fatal(err)
 			panic(err)
 		}
-		func() {
-			out, err := os.Create(fmt.Sprintf("output%d.png", i))
+		if info.IsDir() {
+			return nil
+		}
+		if filepath.Ext(path) != ".S32" {
+			return nil
+		}
+
+		fmt.Printf("name: %s\n", strings.Split(info.Name(), ".")[0])
+		f, err := os.Open(path)
+		if err != nil {
+			log.Fatal(err)
+			panic(err)
+		}
+		defer f.Close()
+
+		sp, err := gosang.OpenSprite(f)
+		if err != nil {
+			log.Fatal(err)
+			panic(err)
+		}
+
+		for i := 0; i < sp.FrameCount(); i++ {
+			frame, err := sp.Frame(i)
 			if err != nil {
+				log.Fatal(err)
 				panic(err)
 			}
-			defer out.Close()
-			if err := png.Encode(out, frame.Image()); err != nil {
-				panic(err)
-			}
-		}()
+			func() {
+				outputDir := fmt.Sprintf(`output\%s`, strings.Split(info.Name(), ".")[0])
+
+				if _, err := os.Stat(outputDir); os.IsNotExist(err) {
+					err := os.MkdirAll(outputDir, os.ModePerm)
+					if err != nil {
+						log.Fatal(err)
+						panic(err)
+					}
+				}
+
+				// System cannot find the path specified. should create new 'output' folder if not exists and replace the path
+				out, err := os.Create(fmt.Sprintf(`%s\%s_%d.png`, outputDir, strings.Split(info.Name(), ".")[0], i))
+				if err != nil {
+					log.Fatal(err)
+					panic(err)
+				}
+				defer out.Close()
+				if err := png.Encode(out, frame.Image()); err != nil {
+					log.Fatal(err)
+					panic(err)
+				}
+			}()
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
 	}
 }
